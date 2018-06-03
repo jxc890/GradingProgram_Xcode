af78c7c212205bf71eaf2bd9f39fdec133baee8b
Jiakai Chen <jxc890@case.edu>
2018-06-03 09:35:59 +0800

    Initial Commit

---
+++ GradingProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GradingProgram
+//
+//  Created by Jiakai Chen on 6/3/18.
+//  Copyright © 2018 Jiakai Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
