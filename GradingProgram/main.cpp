a01c0d32b822db021f3f2bd8a2dea6ff738342e6
Jiakai Chen <jchen154@ucsc.edu>
2018-06-03 11:40:27 +0800

    Version with all the modifications.

--- GradingProgram/main.cpp
+++ GradingProgram/main.cpp
@@ -7,10 +7,81 @@
 //
 
 #include <iostream>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout << "For Testing Commit";
+    int score;
+    cout << "Please enter your score：";
+    cin >> score;
+    
+    // using Switch Statement in C++
+    int i = 0;
+    if (score >= 0 && score <= 59){
+        i = 1;
+    }
+    if (score >= 60 && score <= 69){
+        i = 2;
+    }
+    if (score >= 70 && score <= 79){
+        i = 3;
+    }
+    if (score >= 80 && score <= 99){
+        i = 4;
+    }
+    if (score >= 90 && score <= 100){
+        i = 5;
+    }
+    
+    switch (i) {
+        case 1:
+             cout << "You got an F." << endl;
+            break;
+        case 2:
+             cout << "You got an D." << endl;
+            break;
+        case 3:
+             cout << "You got an C." << endl;
+            break;
+        case 4:
+             cout << "You got an B." << endl;
+            break;
+        case 5:
+             cout << "You got an A." << endl;
+            if (score == 100)
+             cout << "You got the perfect score!" << endl;
+            break;
+        default:
+            cout << "Please enter your correct score!";
+            cin >> score;
+            break;
+    }
+    /*
+    if (score < 0){
+        cout << "Please enter your correct score!";
+        cin >> score;
+    }
+    
+    if (score >= 0 && score <= 59){
+        cout << "You got an F." << endl;
+    }
+    if (score >= 60 && score <= 69){
+        cout << "You got an D." << endl;
+    }
+    if (score >= 70 && score <= 79){
+        cout << "You got an C." << endl;
+    }
+    if (score >= 80 && score <= 89){
+        cout << "You got an B." << endl;
+    }
+    if(score >= 90 && score <= 100){
+        cout << "You got an A." << endl;
+        if (score == 100){
+            cout << "You got the perfect score!" << endl;
+        }
+    }*/
+    
+    //system( "read -n 1 -s -p \"Press any key to continue...\"" );
     return 0;
 }
